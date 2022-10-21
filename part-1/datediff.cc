59f4379ddfa28308613ff314d2e50c8b877d0c65
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-10-21 10:06:40 -0700

    got rid of all the comments

--- part-1/datediff.cc
+++ part-1/datediff.cc
@@ -62,27 +62,3 @@ int main(int argc, char* argv[]) {
   std::cout << " is " << diff << " days\n";
   return 0;
 }
-
-// Given a month, day, and year, calculate the Julian day number using the
-// Fliegel & Van Flandern equation.
-// TODO: write statements to implement this function, and delete this comment
-// TODO: replace this return statement with one that actually works
-// Return the number of days between a start date and end date.
-// TODO: write statements to implement this function, and delete this comment
-// TODO: replace this return statement with one that actually works
-// Print the given date to standard output in the format MM/DD/YYYY
-  // TODO: write statements to implement this function, and delete this comment
-  // TODO: prompt the user for
-  //   - start month
-  //   - start day
-  //   - start year
-  //   - end month
-  //   - end day
-  //   - end year
-  // TODO: call DateDifference, and store its return value in a variable
-  // TODO: output "The number of days between"
-  // TODO: call PrintDate to print out the start date
-  // TODO: output " and "
-  // TODO: call PrintDate to print out the end date
-  // TODO: output " is DAYS days\n", where DAYS is the difference claculated
-  //       by DateDifference above
